160d281511996b486f546e7ccd8cc0e793403df1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-29 12:23:12 -0500

    commit tree 521d64ad7a4a02d9db3a789d2d8ed4e2ddf774e0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${userName}`,
+      email: `${userEmail}`,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document
+        .getElementsByTagName("script")[0]
+        .insertAdjacentHTML("afterend", `${object["id"]}`);
+    })
+    .catch(function (error) {
+      document
+        .getElementsByTagName("script")[0]
+        .insertAdjacentHTML("beforebegin", `${error}`);
+    });
+}
